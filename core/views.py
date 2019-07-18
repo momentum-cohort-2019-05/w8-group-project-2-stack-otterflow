42f1b26c13eb6b1be1419f17920d70216e941e3d
Devin Muhs <devinmuhs@gmail.com>
2019-07-18 12:31:01 -0400

    started favorites views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from core.models import Question, Category, Favorite, Answer, OtterProfile
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     """View function for home page of site."""
@@ -27,3 +28,36 @@ class CategoryDetailView(generic.DetailView):
     
 class OtterProfileDetailView(generic.DetailView):
     model = OtterProfile
+
+@login_required
+def user_favorites(request):
+    favorites = Favorite.objects.filter(favorited_by=request.user)
+
+    favorites_list = []
+
+    for favorite in favorites:
+        favorites_list.append(favorite.question)
+
+    context = {
+        'favorites': favorites,
+        'favorites_list': favorites_list,
+    }
+
+    return render(request, 'core/added_favorites.html', context)
+
+
+@login_required
+def add_to_favorites(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    
+    new_favorite, created = Favorite.objects.get_or_create(question=question, favorited_by=request.user)
+    if not created:
+        new_favorite.delete()
+
+    context = {
+    'question': question,
+    'new_favorite': new_favorite,
+    'created': created,
+    }
+
+    return render(request, 'core/favorite_added.html', context)
